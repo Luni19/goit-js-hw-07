d94e974ad2a5686bc9abc6bd1f0178d474e7d18c
Konstantin <80715972+Luni19@users.noreply.github.com>
2023-09-12 15:21:08 +0300

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,25 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const galleryItem = document.getElementById(gallery);
 
-const galleryLi = galleryItems
-.map(
-    ({preview, original, description}) => 
-    `<li class="gallery__item">
-    <a class="gallery__link" href="${original}">
-    <img class="gallery__image" src= ${preview}" alt="${description}" />
-    </a>
-    </li>`
-)    
-    .join("")
-    gallery.insertAdjacentHTML ('beforeend',  galleryLi)
-    
-    let lightbox = new SimpleLightbox(".gallery a ", {
-        captionsData: "alt",
-        captionDelay: 250
+console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+const markup = createMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", markup);
+
+function createMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
     })
+    .join("");
+}
+
+galleryList.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
 
 
+  const instance = basicLightbox.create(`
+    <div class="modal">
+      <img src=${event.target.dataset.source} alt=${event.target.alt}/>
+    </div>
+  `);
+  instance.show();
 
-
-
+  galleryList.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
